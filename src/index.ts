1e2708bfd6ea933f8044d04565d3c29ad67085f0
Jackson Dorsett <dorsettj@gmail.com>
2024-07-16 11:49:00 -0700

    added common router

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import commonRouter from "./routes/shared";
 import { searchRouter } from "./routes/searchRoutes";
 import { seriesRouter } from "./routes/seriesRoutes";
 import { statusRouter } from "./routes/statusRoutes";
@@ -7,10 +8,13 @@ import { statusRouter } from "./routes/statusRoutes";
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 6968;
-app.use("/search", searchRouter);
-app.use("/", seriesRouter);
-app.use("/status", statusRouter);
 
+app.use("/search", searchRouter);
+
+app.use("/", seriesRouter);
+
+app.use("/status", statusRouter);
+app.use("/shared", commonRouter);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
