58482840e13284e73571e0cf549c662da596f347
Jackson Dorsett <dorsettj@gmail.com>
2024-07-28 12:42:49 -0700

    removed unused status controller

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import cors from "cors";
 import commonRouter from "./routes/shared";
 import { searchRouter } from "./routes/searchRoutes";
 import { seriesRouter } from "./routes/seriesRoutes";
-import { statusRouter } from "./routes/statusRoutes";
 
 const app = express();
 app.use(cors());
@@ -13,8 +12,7 @@ app.use("/search", searchRouter);
 
 app.use("/", seriesRouter);
 
-app.use("/status", statusRouter);
-app.use("/shared", commonRouter);
+app.use("/common", commonRouter);
 app.get("/", (req, res) => {
   res.send("Series Info Service is online.");
 });
