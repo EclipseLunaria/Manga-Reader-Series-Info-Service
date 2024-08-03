c29ef55e11dbec8b35ed813c4b75a4227eaefd2d
Jackson Dorsett <dorsettj@gmail.com>
2024-08-03 10:57:10 -0700

    add manga Id field

--- src/models/seriesModels.ts
+++ src/models/seriesModels.ts
@@ -1,11 +1,12 @@
 export interface MangaSeriesData {
-    title: string;
-    author: string;
-    image: string;
-    rating: { ratingAvg: string; totalVotes: string };
-    description: string;
-    status: string;
-    genres: string[];
-    totalChapters: number;
-    chapters: { title: string; link: string }[];
-  }
+  mangaId: string;
+  title: string;
+  author: string;
+  image: string;
+  rating: { ratingAvg: string; totalVotes: string };
+  description: string;
+  status: string;
+  genres: string[];
+  totalChapters: number;
+  chapters: { title: string; link: string }[];
+}
