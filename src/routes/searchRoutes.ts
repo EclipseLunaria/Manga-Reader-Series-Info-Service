1464c237e662bec2515fa81569e2cec838605d59
Jackson Dorsett <dorsettj@gmail.com>
2024-08-11 08:45:04 -0700

    removed comment

--- src/routes/searchRoutes.ts
+++ src/routes/searchRoutes.ts
@@ -1,9 +1,6 @@
 import { Router } from "express";
 import { findSeries } from "../controllers/searchControllers";
 
-/**
- * Router for handling manga series search functionality.
- */
 export const searchRouter = Router();
 
 searchRouter.get("/", findSeries);
